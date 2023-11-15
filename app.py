6ebd178921d3e631263c298b7678b5f961cbe9e3
AntaresBermuda <55585693+AntaresBermuda@users.noreply.github.com>
2023-11-15 12:36:58 -0400

    eff

--- app.py
+++ app.py
@@ -8,11 +8,10 @@ import seaborn as sns
 from dateutil.relativedelta import relativedelta
 import time
 
-st.set_page_config(page_title="Antares Bermuda")
-st.header("FreqSevModel", divider="rainbow")
+st.set_page_config(page_title="League")
 
-
-
+st.header("FreqSevModel")
+st.subheader("", divider="rainbow")
 
 st.subheader("If you were sent to war, how would you fight your opponents? Pick 3.")
 
