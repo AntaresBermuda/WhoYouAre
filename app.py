e4271d048b0ea49b3d61e14f050c3f218160d111
AntaresBermuda <55585693+AntaresBermuda@users.noreply.github.com>
2023-11-15 11:56:43 -0400

    tgg

--- app.py
+++ app.py
@@ -10,3 +10,7 @@ import time
 
 st.set_page_config(page_title="Antares Bermuda")
 st.title("Frequency-Severity Modelling")
+
+Q1 = st.selectbox(
+    'If you were sent to war, how would you fight your opponents?',
+    ("Machine Gun", "Remote controlled bomber", "Sniper", "Tank Opperator", "Swarm of small AI-powered drones", "Assasinate Everyone with a Dagger", "Laser Gun", "Lightsaber", "Trowing Axes"))
