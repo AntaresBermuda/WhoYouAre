cbb0a2b05d199d7f6337dbcc93e11cc769dce3d7
AntaresBermuda <55585693+AntaresBermuda@users.noreply.github.com>
2023-11-15 12:17:07 -0400

    wdd

--- app.py
+++ app.py
@@ -15,8 +15,7 @@ Q1 = st.selectbox(
     'If you were sent to war, how would you fight your opponents? Pick 3.',
     ("-", "Machine Gun", "Remote controlled Drones", "FN P90, I'm cultured.", "Sniper", "Tank Opperator", "Throw Grenades", "Swarm of AI-Powered Drones", "Assasinate with a Dagger", "Bow and Arrow","Laser Gun", "Lightsaber", "Trowing Axes", "Nuke Them", "I wouldn't fight, I'm a peaceful person."))
 
-Q1_1 = st.selectbox(
-    '',
+Q1_1 = st.selectbox("",
     ("-", "Machine Gun", "Remote controlled Drones", "FN P90, I'm cultured.", "Sniper", "Tank Opperator", "Throw Grenades", "Swarm of AI-Powered Drones", "Assasinate with a Dagger", "Bow and Arrow","Laser Gun", "Lightsaber", "Trowing Axes", "Nuke Them", "I wouldn't fight, I'm a peaceful person."))
 
 
