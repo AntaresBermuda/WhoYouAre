b4b6d7e87e1ed27b07a6acc3a531262b3fe4cb58
AntaresBermuda <55585693+AntaresBermuda@users.noreply.github.com>
2023-11-15 12:16:16 -0400

    efef

--- app.py
+++ app.py
@@ -12,8 +12,13 @@ st.set_page_config(page_title="Antares Bermuda")
 st.title("Frequency-Severity Modelling")
 
 Q1 = st.selectbox(
-    'If you were sent to war, how would you fight your opponents?',
+    'If you were sent to war, how would you fight your opponents? Pick 3.',
     ("-", "Machine Gun", "Remote controlled Drones", "FN P90, I'm cultured.", "Sniper", "Tank Opperator", "Throw Grenades", "Swarm of AI-Powered Drones", "Assasinate with a Dagger", "Bow and Arrow","Laser Gun", "Lightsaber", "Trowing Axes", "Nuke Them", "I wouldn't fight, I'm a peaceful person."))
+
+Q1_1 = st.selectbox(
+    '',
+    ("-", "Machine Gun", "Remote controlled Drones", "FN P90, I'm cultured.", "Sniper", "Tank Opperator", "Throw Grenades", "Swarm of AI-Powered Drones", "Assasinate with a Dagger", "Bow and Arrow","Laser Gun", "Lightsaber", "Trowing Axes", "Nuke Them", "I wouldn't fight, I'm a peaceful person."))
+
 
 Q2 = st.selectbox(
     'Which describes you best, pick 3?',
