c7cb0e97b11c2a20b01d8b2c79608c62e9913413
AntaresBermuda <55585693+AntaresBermuda@users.noreply.github.com>
2023-11-15 12:34:00 -0400

    ef

--- app.py
+++ app.py
@@ -9,14 +9,20 @@ from dateutil.relativedelta import relativedelta
 import time
 
 st.set_page_config(page_title="Antares Bermuda")
-st.title("Frequency-Severity Modelling")
+st.header("FreqSevModel", divider="rainbow")
 
-st.subheader("If you were sent to war, how would you fight your opponents? Pick 3.")
+
+st.subheader("If you were sent to war, how would you fight your opponents? Pick 3.", divider="rainbow")
 
 Q1 = st.selectbox("",
     ("-", "Machine Gun", "Remote controlled Drones", "FN P90, I'm cultured.", "Sniper", "Tank Opperator", 
     "Throw Grenades", "Swarm of AI-Powered Drones", "Assasinate with a Dagger", "Bow and Arrow","Laser Gun", "Lightsaber", "Trowing Axes", "Nuke Them", "I wouldn't fight, I'm a peaceful person."),
     key="q1")
+
+Q1_1 = st.selectbox("",
+    ("-", "Machine Gun", "Remote controlled Drones", "FN P90, I'm cultured.", "Sniper", "Tank Opperator", 
+    "Throw Grenades", "Swarm of AI-Powered Drones", "Assasinate with a Dagger", "Bow and Arrow","Laser Gun", "Lightsaber", "Trowing Axes", "Nuke Them", "I wouldn't fight, I'm a peaceful person."),
+    key="q1_1")
 
 Q1_1 = st.selectbox("",
     ("-", "Machine Gun", "Remote controlled Drones", "FN P90, I'm cultured.", "Sniper", "Tank Opperator", 
