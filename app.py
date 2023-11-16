71a8c994f306012e214d6aaa0c53d2f0a81b92ce
AntaresBermuda <55585693+AntaresBermuda@users.noreply.github.com>
2023-11-16 10:24:15 -0400

    fef

--- app.py
+++ app.py
@@ -31,6 +31,8 @@ Q1_2 = st.selectbox("",
     "Throw Grenades", "Swarm of AI-Powered Drones", "Assasinate them at Night", "Satelite Space Lasers", "Slash them with an Axe", "Drop a Nuke", "I wouldn't fight, I'm a peaceful person."),
     key="q1_2")
 
+
+st.write('\n')
 st.subheader("", divider="rainbow")
 st.write('\n')
 st.subheader("Which Positive Traits describes you best? Pick 3.")
@@ -48,7 +50,7 @@ Q2_2 = st.selectbox("",
     ("-", "Strategic", "Skillful", "Athletic", "Brute Force", "Helpful", "Patient", "Powerful", "Sneaky", "Genius", "Talented", "Unpredictable", "Rational", "Friendly", "Efficient", "Productive", "Optimistic", "Modest"),
     key="q2_2")
 
-
+st.write('\n')
 st.subheader("", divider="rainbow")
 st.write('\n')
 st.subheader("Which Negative Traits describes you best? Pick 2.")
@@ -61,7 +63,7 @@ Q3_1 = st.selectbox("",
     ("-", "Impatient", "Revengeful", "Just Crazy", "Pessimistic", "Angry", "Paranoid", "Lazy", "Selfish", "boastful", "Controlling"),
     key="q3_1")
 
-
+st.write('\n')
 st.subheader("", divider="rainbow")
 st.write('\n')
 st.subheader("What Weapons do you keep just in case. Pick 2.")
@@ -75,7 +77,7 @@ Q4_1 = st.selectbox("",
     ("-", "Two handguns", "My Fists", "Taser Gun", "FN P90, I'm cultured", "A Mdieval Sword", "An ancient book with Magical Spells", "Bow and Arrow", "Lightsaber", "bazooka"),
     key="q4_1")
 
-
+st.write('\n')
 st.subheader("", divider="rainbow")
 st.write('\n')
 st.subheader("What Superpowers would you like to have? Pick 3.")
@@ -90,7 +92,7 @@ Q5_2 = st.selectbox("",
     ("-", "Indestructible", "Super Strenght", "Teleportation", "Healing/Reviving Powers", "Invisability", "Never Age", "X-ray Vision", "Time-Travel", "Shape-Shifting", "Extreme Intelligence", "Sonic Powers", "Ultra Speed", "Lighting/Electric Powers", "Fire-Bending", "Water-Bending", "Air-Bending", "Earth-Bending", "Magical Spells", "I don't need Superpowers"),
     key="q5_2")
 
-
+st.write('\n')
 st.subheader("", divider="rainbow")
 st.write('\n')
 st.subheader("Choose 2 pets.")
